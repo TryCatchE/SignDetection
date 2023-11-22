7fb3c024ba5b436c9b762f85dc929a77115a6712
JoMds <Giannis58@Hotmail.com>
2023-11-22 11:02:08 +0200

    model created and tested

---
+++ runModel.py
@@ -0,0 +1,74 @@
+import pickle
+
+import cv2
+import mediapipe as mp
+import numpy as np
+
+modelDir = pickle.load(open('./model.p', 'rb'))
+model = modelDir['model']
+
+cap = cv2.VideoCapture(0)
+
+hands = mp.solutions.hands
+drawUtls = mp.solutions.drawing_utils
+styleUtls = mp.solutions.drawing_styles
+
+handsObj = hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
+
+labels = {0: 'A', 1: 'L', 2: 'B'}
+while True:
+
+    handEdges = []
+    xList = []
+    yList = []
+
+    ret, frame = cap.read()
+
+    H, W, _ = frame.shape
+
+    vFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    results = handsObj.process(vFrame)
+    if results.multi_hand_landmarks:
+        for handCordinates in results.multi_hand_landmarks:
+            drawUtls.draw_landmarks(
+                frame,  # image to draw
+                handCordinates,  # model output
+                hands.HAND_CONNECTIONS,  # hand connections
+                styleUtls.get_default_hand_landmarks_style(),
+                styleUtls.get_default_hand_connections_style())
+
+        for handCordinates in results.multi_hand_landmarks:
+            for i in range(len(handCordinates.landmark)):
+                x = handCordinates.landmark[i].x
+                y = handCordinates.landmark[i].y
+
+                xList.append(x)
+                yList.append(y)
+
+            for i in range(len(handCordinates.landmark)):
+                x = handCordinates.landmark[i].x
+                y = handCordinates.landmark[i].y
+                handEdges.append(x - min(xList))
+                handEdges.append(y - min(yList))
+
+        x1 = int(min(xList) * W) - 10
+        y1 = int(min(yList) * H) - 10
+
+        x2 = int(max(xList) * W) - 10
+        y2 = int(max(yList) * H) - 10
+
+        prediction = model.predict([np.asarray(handEdges)])
+
+        predLabel = labels[int(prediction[0])]
+
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 0), 4)
+        cv2.putText(frame, predLabel, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 0, 0), 3,
+                    cv2.LINE_AA)
+
+    cv2.imshow('frame', frame)
+    cv2.waitKey(1)
+
+
+cap.release()
+cv2.destroyAllWindows()
